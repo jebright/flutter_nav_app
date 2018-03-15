96015fd3d068deebcaa470fd51fa099db9b66bf8
joe <ebrightster@gmail.com>
2018-03-15 05:46:04 -0700

    Adding handler to prevent leaving app without confirming

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'about.dart';
 
 class MyHomePage extends StatefulWidget {
-  
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
@@ -12,10 +13,32 @@ void _navAbout(BuildContext context) {
       context, new MaterialPageRoute(builder: (context) => new AboutPage()));
 }
 
+Future<bool> _exitApp( BuildContext context) {
+  return showDialog(
+      context: context,
+      child: new AlertDialog(
+        title: new Text('Do you want to exit this application?'),
+        content: new Text('We hate to see you leave...'),
+        actions: <Widget>[
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: new Text('No'),
+          ),
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: new Text('Yes'),
+          ),
+        ],
+      ),
+    ) ?? false; 
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return new WillPopScope(
+        onWillPop: () => _exitApp(context),
+        child: new Scaffold(
       appBar: new AppBar(
         title: new Text('Home Page'),
       ),
@@ -25,6 +48,6 @@ class _MyHomePageState extends State<MyHomePage> {
           onPressed: () => _navAbout(context),
         ),
       ),
-    );
+    ));
   }
 }
